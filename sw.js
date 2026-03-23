59b3d80867d08a2e6dcdfaa693180adc3d1c0ef5
kentaro222 <magdracky@gmail.com>
2026-03-23 14:49:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,6 +11,23 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// アクティブ化：古いキャッシュを掃除する
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          // 現在の CACHE_NAME 以外は削除する
+          if (cacheName !== CACHE_NAME) {
+            console.log('古いキャッシュを削除しました:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
